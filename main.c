c6347c76979602fc1b8d5ba89f19a5635c7ba7be
mlikhithesh-afk <mlikhithesh@gmail.com>
2026-06-08 15:47:12 +0530

    Refactor drawing functions and update menu options

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 #define ROWS 20
@@ -13,7 +14,7 @@ void initCanvas()
     {
         for(j = 0; j < COLS; j++)
         {
-            canvas[i][j] = '-';
+            canvas[i][j] = '.';
         }
     }
 }
@@ -40,7 +41,10 @@ void drawRectangle(int x, int y, int width, int height)
     {
         for(j = 0; j < width; j++)
         {
-            canvas[y + i][x + j] = '*';
+            if(y + i < ROWS && x + j < COLS)
+            {
+                canvas[y + i][x + j] = '*';
+            }
         }
     }
 }
@@ -49,9 +53,9 @@ void drawLine(int y, int x1, int x2)
 {
     int i;
 
-    for(i = x1; i <= x2; i++)
+    for(i = x1; i <= x2 && i < COLS; i++)
     {
-        canvas[y][i] = '#';
+        canvas[y][i] = '*';
     }
 }
 
@@ -63,9 +67,45 @@ void drawTriangle(int x, int y, int height)
     {
         for(j = 0; j <= i; j++)
         {
-            canvas[y + i][x + j] = '^';
+            if(y + i < ROWS && x + j < COLS)
+            {
+                canvas[y + i][x + j] = '^';
+            }
         }
     }
+}
+
+void drawCircle(int cx, int cy, int r)
+{
+    int x, y;
+
+    for(y = 0; y < ROWS; y++)
+    {
+        for(x = 0; x < COLS; x++)
+        {
+            int dx = x - cx;
+            int dy = y - cy;
+
+            if(dx * dx + dy * dy <= r * r)
+            {
+                canvas[y][x] = 'O';
+            }
+        }
+    }
+}
+
+void deletePicture()
+{
+    initCanvas();
+    printf("Picture Cleared!\n");
+}
+
+void modifyRectangle()
+{
+    deletePicture();
+    drawRectangle(15, 8, 12, 5);
+
+    printf("Rectangle Modified!\n");
 }
 
 int main()
@@ -80,10 +120,13 @@ int main()
         printf("1. Draw Rectangle\n");
         printf("2. Draw Line\n");
         printf("3. Draw Triangle\n");
-        printf("4. Display Canvas\n");
-        printf("5. Exit\n");
+        printf("4. Draw Circle\n");
+        printf("5. Display Canvas\n");
+        printf("6. Delete Picture\n");
+        printf("7. Modify Rectangle\n");
+        printf("8. Exit\n");
 
-        printf("Entoice: ");
+        printf("Enter Choice: ");
         scanf("%d", &choice);
 
         switch(choice)
@@ -94,7 +137,7 @@ int main()
                 break;
 
             case 2:
-                drawLine(2, 5, 20);
+                drawLine(2, 5, 25);
                 printf("Line Drawn!\n");
                 break;
 
@@ -104,10 +147,23 @@ int main()
                 break;
 
             case 4:
-                displayCanvas();
+                drawCircle(30, 12, 3);
+                printf("Circle Drawn!\n");
                 break;
 
             case 5:
+                displayCanvas();
+                break;
+
+            case 6:
+                deletePicture();
+                break;
+
+            case 7:
+                modifyRectangle();
+                break;
+
+            case 8:
                 printf("Exiting...\n");
                 return 0;
 
